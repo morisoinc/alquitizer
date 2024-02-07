7ce153ac3eb4574190eac411ee45d1db7bbb574d
morisoinc <64343733+morisoinc@users.noreply.github.com>
2024-02-07 15:49:50 -0300

    Initial Commit

---
+++ alquitizer/alquitizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  alquitizerApp.swift
+//  alquitizer
+//
+//  Created by Mauricio Fontana on 07/02/24.
+//
+
+import SwiftUI
+
+@main
+struct alquitizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
