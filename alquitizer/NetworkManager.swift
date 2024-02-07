d7dc3504827bab38731ae197621ff8b5557516dc
morisoinc <64343733+morisoinc@users.noreply.github.com>
2024-02-07 16:39:42 -0300

    added network manager singleton with getAppetizer method, added APError

---
+++ alquitizer/NetworkManager.swift
@@ -0,0 +1,53 @@
+//
+//  NetworkManager.swift
+//  alquitizer
+//
+//  Created by Mauricio Fontana on 07/02/24.
+//
+
+import Foundation
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    
+    static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
+    private let appetizerURL = baseURL + "appetizers"
+
+    private init() {}
+
+    func getAppetizers(completed: @escaping (Result<[Appetizer], APError>) -> Void) {
+
+        guard let url = URL(string: appetizerURL) else {
+            completed(.failure(.invalidURL))
+            return
+        }
+
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let _ = error {
+                completed(.failure(.unableToComplete))
+                return
+            }
+
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completed(.failure(.invalidResponse))
+                return
+            }
+
+            guard let data = data else {
+                completed(.failure(.invalidData))
+                return
+            }
+
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let decodedResponse = try decoder.decode(AppetizerResponse.self, from: data)
+                completed(.success(decodedResponse.request))
+            } catch {
+                completed(.failure(.invalidData))
+            }
+        }
+
+        task.resume()
+    }
+}
