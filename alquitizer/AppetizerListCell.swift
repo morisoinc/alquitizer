91b3040b361e301f77ea188593a4d5904f6a5c7b
morisoinc <64343733+morisoinc@users.noreply.github.com>
2024-02-08 13:10:10 -0300

    updated image in appetizer list cell with asyncimage

--- alquitizer/AppetizerListCell.swift
+++ alquitizer/AppetizerListCell.swift
@@ -8,17 +8,19 @@
 import SwiftUI
 
 struct AppetizerListCell: View {
-    
     let appetizer: Appetizer
-    
+
     var body: some View {
         HStack {
-            Image(appetizer.imageURL)
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 120, height: 90)
-                .cornerRadius(8)
-            
+            AsyncImage(url: URL(string: appetizer.imageURL)) { image in
+                image.resizable()
+                    .aspectRatio(contentMode: .fill)
+            } placeholder: {
+                ProgressView()
+            }
+            .frame(width: 120, height: 90)
+            .cornerRadius(8)
+
             VStack(alignment: .leading, spacing: 5) {
                 Text(appetizer.name)
                     .font(.title2)
