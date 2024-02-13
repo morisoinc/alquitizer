d547564a8159ec0bc1ea0eac3f3a040848ae2f97
morisoinc <64343733+morisoinc@users.noreply.github.com>
2024-02-12 21:17:25 -0300

    cleaned up guard unwrap, replaced email regex with regex builder

--- alquitizer/Utilities/Extensions/String+Ext.swift
+++ alquitizer/Utilities/Extensions/String+Ext.swift
@@ -6,11 +6,37 @@
 //
 
 import Foundation
+import RegexBuilder
 
 extension String {
     var isValidEmail: Bool {
-        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let emailPred = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
-        return emailPred.evaluate(with: self)
+        let emailRegEx = Regex {
+            OneOrMore {
+                CharacterClass(
+                    .anyOf("._%+-"),
+                    ("A"..."Z"),
+                    ("0"..."9"),
+                    ("a"..."z")
+                )
+            }
+            "@"
+            OneOrMore {
+                CharacterClass(
+                    .anyOf(".-"),
+                    ("A"..."Z"),
+                    ("a"..."z"),
+                    ("0"..."9")
+                )
+            }
+            "."
+            Repeat(2...64) {
+                CharacterClass(
+                    ("A"..."Z"),
+                    ("a"..."z")
+                )
+            }
+        }
+        
+        return self.wholeMatch(of: emailRegEx) != nil
     }
 }
