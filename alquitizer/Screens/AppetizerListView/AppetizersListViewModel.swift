8b61017e47f3401b39d256ce5f1c0f3fc22155e0
morisoinc <64343733+morisoinc@users.noreply.github.com>
2024-02-12 21:03:41 -0300

    replaced old escaping logic with async/await and mainactor

--- alquitizer/Screens/AppetizerListView/AppetizersListViewModel.swift
+++ alquitizer/Screens/AppetizerListView/AppetizersListViewModel.swift
@@ -8,7 +8,8 @@
 import Foundation
 import SwiftUI
 
-class AppetizersListViewModel: ObservableObject {
+
+@MainActor class AppetizersListViewModel: ObservableObject {
     @Published var appetizers: [Appetizer] = []
     @Published var alertItem: AlertItem?
     @Published var isLoading: Bool = false
@@ -21,25 +22,29 @@ class AppetizersListViewModel: ObservableObject {
 
     func getAppetizers() {
         isLoading = true
-        NetworkManager.shared.getAppetizers { result in
-            DispatchQueue.main.async {
-                self.isLoading = false
-                switch result {
-                case .success(let appetizers):
-                    self.appetizers = appetizers
-                case .failure(let error):
-                    switch error {
-                    case .invalidData:
-                        self.alertItem = AlertContext.invalidData
-                    case .invalidResponse:
-                        self.alertItem = AlertContext.invalidResponse
+
+        Task {
+            do {
+                appetizers = try await NetworkManager.shared.getAppetizers()
+            } catch {
+                if let apError = error as? APError {
+                    switch apError {
                     case .invalidURL:
-                        self.alertItem = AlertContext.invalidURL
+                        alertItem = AlertContext.invalidURL
+                    case .invalidResponse:
+                        alertItem = AlertContext.invalidResponse
+                    case .invalidData:
+                        alertItem = AlertContext.invalidData
                     case .unableToComplete:
-                        self.alertItem = AlertContext.unableToComplete
+                        alertItem = AlertContext.unableToComplete
                     }
+                } else {
+                    alertItem = AlertContext.invalidData
                 }
+
             }
+
+            isLoading = false
         }
     }
 }
--- alquitizer/Utilities/Managers/NetworkManager.swift
+++ alquitizer/Utilities/Managers/NetworkManager.swift
@@ -9,45 +9,24 @@ import Foundation
 
 final class NetworkManager {
     static let shared = NetworkManager()
-    
+
     static let baseURL = "https://seanallen-course-backend.herokuapp.com/swiftui-fundamentals/"
     private let appetizerURL = baseURL + "appetizers"
 
     private init() {}
 
-    func getAppetizers(completed: @escaping (Result<[Appetizer], APError>) -> Void) {
-
+    func getAppetizers() async throws -> [Appetizer] {
         guard let url = URL(string: appetizerURL) else {
-            completed(.failure(.invalidURL))
-            return
+            throw APError.invalidURL
         }
 
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let _ = error {
-                completed(.failure(.unableToComplete))
-                return
-            }
+        let (data, _) = try await URLSession.shared.data(from: url)
 
-            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completed(.failure(.invalidResponse))
-                return
-            }
-
-            guard let data = data else {
-                completed(.failure(.invalidData))
-                return
-            }
-
-            do {
-                let decoder = JSONDecoder()
-                decoder.keyDecodingStrategy = .convertFromSnakeCase
-                let decodedResponse = try decoder.decode(AppetizerResponse.self, from: data)
-                completed(.success(decodedResponse.request))
-            } catch {
-                completed(.failure(.invalidData))
-            }
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode(AppetizerResponse.self, from: data).request
+        } catch {
+            throw APError.invalidData
         }
-
-        task.resume()
     }
 }
